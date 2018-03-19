21fa723047283c2a7e7a1fc8d2dff4abc2c7b178
Viktoria Jechsmayr <viktoria@jechsmayr.at>
2018-03-19 11:30:24 +0100

    Revert "jquery-paginate und pagination deinstalliert"
    This reverts commit 7ef883e1fc260794436aa1e8978daff14f86f3a1.

---
+++ SourcingReporting_NodeJS/SourcingReporting_NodeJS/public/js/sourcing/loadingScreen.js
@@ -0,0 +1,18 @@
+﻿function onReady(callback) {
+    var intervalId = window.setInterval(function () {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalId);
+            callback.call(this);
+        }
+    }, 1000);
+}
+
+function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function () {
+    setVisible('#candidates', true);
+    setVisible('#loadingCircles', false);
+    //$('#candidates').paginate({ limit: 50 });
+});
